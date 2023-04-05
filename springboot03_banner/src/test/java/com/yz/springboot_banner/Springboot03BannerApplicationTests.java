fcb6612a3f997395d2eb9ccd266fa1fe6af7f17a
MrYz0 <2569901191@qq.com>
2023-04-05 21:31:32 +0800

    SpringBoot入门 - 定制自己的Banner

---
+++ springboot03_banner/src/test/java/com/yz/springboot_banner/Springboot03BannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.springboot_banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03BannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
