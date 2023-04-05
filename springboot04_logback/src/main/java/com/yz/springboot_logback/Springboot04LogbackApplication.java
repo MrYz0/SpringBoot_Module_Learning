8c09e02b4e09b8a472aaeecc560c7fac4d91437a
MrYz0 <2569901191@qq.com>
2023-04-05 21:42:05 +0800

    SpringBoot入门 - 添加Logback日志

---
+++ springboot04_logback/src/main/java/com/yz/springboot_logback/Springboot04LogbackApplication.java
@@ -0,0 +1,13 @@
+package com.yz.springboot_logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04LogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04LogbackApplication.class, args);
+	}
+
+}
