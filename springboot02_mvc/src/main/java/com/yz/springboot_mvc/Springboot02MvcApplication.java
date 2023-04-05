dbadaca35fdca80b0190044e7e9ab502b909bc42
MrYz0 <2569901191@qq.com>
2023-04-05 21:07:36 +0800

    SpringBoot入门 - 对Hello world进行MVC分层

---
+++ springboot02_mvc/src/main/java/com/yz/springboot_mvc/Springboot02MvcApplication.java
@@ -0,0 +1,13 @@
+package com.yz.springboot_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02MvcApplication.class, args);
+	}
+
+}
---
+++ springboot02_mvc/src/test/java/com/yz/springboot_mvc/Springboot02MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.springboot_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
